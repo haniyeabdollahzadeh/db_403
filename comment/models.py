f83d25fb1f8da4f917008e1b230d0d41d515ec61
haniyeabdollahzadeh <haniyehabdollahzadeh0gmail.com>
2025-01-06 11:43:00 +0330

    create Notification models

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+class Poll(models.Model):
+    poll_number = models.IntegerField()
+    text = models.TextField()
+
+
+class Poll_comment(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Notification(models.Model):
+    number = models.IntegerField()
+    text = models.TextField()
+
+
+
+class Notification_User(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    number = models.ForeignKey(Notification, on_delete=models.CASCADE)
+    
