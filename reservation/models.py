d063a486dcb56d36104ebb2af5565433faa8c60c
haniyeabdollahzadeh <haniyehabdollahzadeh0gmail.com>
2025-01-06 11:22:19 +0330

    create model reservation

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.FloatField()
+
+
+class reservation(models.Model):
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    food_name = models.ForeignKey(Food, on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
+
