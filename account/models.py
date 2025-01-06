ab15dbcbf47b1a7f37993db2905aa069126db2ea
haniyeabdollahzadeh <haniyehabdollahzadeh0gmail.com>
2025-01-06 11:23:31 +0330

    create modle user

--- account/models.py
+++ account/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class User (models.Model):
+
+    LEVEL_CHOICES = (
+        ("A", "admin"),
+        ("U", "user")
+    )
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=15)
+    access_level = models.CharField(max_length=1, choices=LEVEL_CHOICES)
+    credit = models.FloatField()
+
+ 
+
+  
+
+
