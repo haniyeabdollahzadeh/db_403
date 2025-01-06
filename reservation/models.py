a51e417a979525bfd2573d849892b3b77e59fd92
Baqer Marani <60050143+baqermarani@users.noreply.github.com>
2025-01-06 14:59:01 +0330

    Update models.py after change

--- reservation/models.py
+++ reservation/models.py
@@ -6,12 +6,17 @@ from account.models import User
 class Food(models.Model):
     name = models.CharField(max_length=20)
     price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
 
 class Reservation(models.Model):
     username = models.ForeignKey(User,on_delete=models.CASCADE)
     food_name = models.ForeignKey(Food, on_delete=models.CASCADE)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 
