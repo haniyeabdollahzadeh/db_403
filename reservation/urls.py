49d1d613e45fa9354bf93393c47c456a323752d7
haniyeabdollahzadeh <140152630+haniyeabdollahzadeh@users.noreply.github.com>
2025-01-07 17:20:27 +0330

    add url auth_view.LoginView

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('food/create/', views.food_creat, name='food_create'),
     path('foods/', views.food_list, name='food_list'),
     path('food/reserve/', views.reservation_create, name='reservation_create'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
