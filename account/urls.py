a7f606edb679e86950e3c1836330b046b556392e
haniyeabdollahzadeh <140152630+haniyeabdollahzadeh@users.noreply.github.com>
2025-01-08 20:33:09 +0330

    add custom_login url

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),  # مسیر ثبت‌نام
+    path('signup/', views.signup, name='signup'),# مسیر ثبت‌نام
+      path('custom-login/', views.custom_login, name='custom_login'),  
     
 ]
 
