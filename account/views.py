c8ac0e564905b1b4e8e78c4ce9e5023e9aec7eed
haniyeabdollahzadeh <140152630+haniyeabdollahzadeh@users.noreply.github.com>
2025-01-08 08:50:25 +0330

    move signup to account

--- account/views.py
+++ account/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from account.forms import CustomUserCreationForm
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  # ثبت‌نام کاربر
+            messages.success(request, 'حساب کاربری شما با موفقیت ایجاد شد!')
+            return redirect('login')  # بعد از ثبت‌نام به صفحه لاگین هدایت می‌شود
+        else:
+            messages.error(request, 'لطفاً فرم را به درستی پر کنید.')
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'registration/signup.html', {'form': form})
