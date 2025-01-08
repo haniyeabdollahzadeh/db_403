69fb1065e179c9d60bcf8bcfb6d7f90c05c501a7
haniyeabdollahzadeh <140152630+haniyeabdollahzadeh@users.noreply.github.com>
2025-01-08 19:40:14 +0330

    create views poll_list, poll_detail, poll_create, poll_delete

--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Poll, Poll_comment
+from .forms import PollForm
 
-# Create your views here.
+def poll_list(request):
+    polls = Poll.objects.all()
+    return render(request, 'poll_list.html', {'polls' : polls})
+
+def poll_detail(request, poll_id):
+    poll = get_object_or_404(Poll, id=poll_id)
+    return render(request, 'poll_detail.htm', {'poll': poll})
+
+
+def poll_created(request):
+    if request.method == 'POST':
+        form = PollForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('poll-list')
+    form = PollForm()
+    return render(request, 'poll_form.html', {'form' : form})
+    
+def poll_delete(request, poll_id):
+    poll = get_object_or_404(Poll, id=poll_id)
+    poll.delete()
+    return redirect('poll-list')
